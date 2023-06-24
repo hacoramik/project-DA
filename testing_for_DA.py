cfc14544236c6b9e03a954117a53067fb2679c31
Эдуард <gerasimenko.edm@gmail.com>
2023-06-24 17:34:53 +1000

    График для температуры обновлен, теперь не только средняя, но и min с max. + возможность погоды на 3 месяца

--- testing_for_DA.py
+++ testing_for_DA.py
@@ -43,13 +43,13 @@ bot = telebot.TeleBot(TOKEN)
 # Обработчик команды /start
 @bot.message_handler(commands=['start'])
 def start_message(message):
-    bot.send_message(message.chat.id, "Привет! Чтобы узнать погоду, введите название города:")
+    bot.send_message(message.chat.id, "Привет! Для того, чтобы узнать функционал - напишите /help. \nЧтобы узнать погоду, введите название города:")
 
 # Добавляем обработчик для кнопки "вернуться к выбору города"
 @bot.message_handler(func=lambda message: message.text == 'Вернуться к выбору города')
 def back_to_city(message):
     # Создаем клавиатуру и настройки кнопок с городами
-    keyboard = types.ReplyKeyboardMarkup(row_width=2, resize_keyboard=True)
+    keyboard = types.ReplyKeyboardMarkup(row_width=3, resize_keyboard=True)
     buttons = [types.KeyboardButton(city) for city in cities]
     keyboard.add(*buttons)
 
@@ -58,6 +58,12 @@ def back_to_city(message):
 
     # Регистрируем обработчик нажатия на кнопки города
     bot.register_next_step_handler(message, choose_city)
+
+@bot.message_handler(commands=['help'])
+def help_message(message):
+    bot.send_message(message.chat.id,
+                     "Пока что это прикол, инфа будет позже")
+
 
 # Обработчик ввода названия города
 @bot.message_handler(func=lambda message: True)
@@ -101,10 +107,11 @@ def choose_param(message):
         button2 = types.KeyboardButton(text="На 3 дня")
         button3 = types.KeyboardButton(text="На неделю")
         button4 = types.KeyboardButton(text="На месяц")
-        button5 = types.KeyboardButton(text="На год")
-        button6 = types.KeyboardButton(text="Вернуться к выбору города")
+        button5 = types.KeyboardButton(text="На 3 месяца")
+        button6 = types.KeyboardButton(text="На год")
+        button7 = types.KeyboardButton(text="Вернуться к выбору города")
 
-        keyboard.add(button, button1, button2, button3, button4, button5, button6)
+        keyboard.add(button, button1, button2, button3, button4, button5, button6, button7)
 
         # Отправляем сообщение с выбором периода и клавиатурой
         bot.send_message(message.chat.id, f"Выбери период для {param}:", reply_markup=keyboard)
@@ -141,6 +148,9 @@ def get_weather(message):
         elif period == "На месяц":
             start_date = datetime.now().replace(hour=0, minute=0, second=0) + timedelta(days=1)
             end_date = datetime.now().replace(hour=0, minute=0, second=0) + timedelta(days=32)
+        elif period == "На 3 месяца":
+            start_date = datetime.now().replace(hour=0, minute=0, second=0) + timedelta(days=1)
+            end_date = datetime.now().replace(hour=0, minute=0, second=0) + timedelta(days=94)
         elif period == "На год":
             start_date = datetime.now().replace(hour=0, minute=0, second=0) + timedelta(days=1)
             end_date = datetime.now().replace(hour=0, minute=0, second=0) + timedelta(days=366)
@@ -154,9 +164,9 @@ def get_weather(message):
         # Отбираем нужный параметр из данных о погоде
         if param == "Температура":
             # Готовим данные для модели
-            forecast_data = data.rename(columns={"date": "ds",
-                                                 "tavg": "y"})
+            forecast_data = data[["date", "tmin", "tmax", "tavg"]].rename(columns={"date": "ds"})
             y_label = "Температура, °C"
+
         elif param == "Осадки":
             forecast_data = data.rename(columns={"date": "ds",
                                                  "prcp": "y"})
@@ -166,11 +176,28 @@ def get_weather(message):
             forecast_data = data.rename(columns={"date": "ds",
                                                  "pres": "y"})
 
-        # Инициализируем прогнозную модель
-        model = Prophet()
+        if param == 'Температура':
+            # Инициализируем прогнозную модель для tmin
+            model_tmin = Prophet()
+            # Обучаем модель
+            model_tmin.fit(forecast_data[["ds", "tmin"]].rename(columns={"tmin": "y"}))
 
-        # Обучаем модель
-        model.fit(forecast_data)
+            # Инициализируем прогнозную модель для tmax
+            model_tmax = Prophet()
+            # Обучаем модель
+            model_tmax.fit(forecast_data[["ds", "tmax"]].rename(columns={"tmax": "y"}))
+
+            # Инициализируем прогнозную модель для tavg
+            model_tavg = Prophet()
+            # Обучаем модель
+            model_tavg.fit(forecast_data[["ds", "tavg"]].rename(columns={"tavg": "y"}))
+
+        else:
+            # Инициализируем прогнозную модель
+            model = Prophet()
+
+            # Обучаем модель
+            model.fit(forecast_data)
 
         # Получаем даты для прогноза
         if period == "На cегодня":
@@ -183,26 +210,56 @@ def get_weather(message):
             future_dates = pd.date_range(start=datetime.now(), end=end_date, freq='H')
         elif period == "На месяц":
             future_dates = pd.date_range(start=datetime.now(), end=end_date, freq='D')
+        elif period == "На 3 месяца":
+            future_dates = pd.date_range(start=datetime.now(), end=end_date, freq='D')
         elif period == "На год":
             future_dates = pd.date_range(start=datetime.now(), end=end_date, freq='D')
         elif period == "Вернуться к выбору города":
             back_to_city(message)
             return
 
-        # Получаем прогноз
-        future = model.predict(pd.DataFrame({"ds": future_dates}))
-        print('here')
-        # Рисуем график с историческими данными и прогнозом
-        fig = go.Figure()
-        # fig.add_trace(go.Scatter(x=weather_data.index, y=parameter.values, name='Исторические данные'))
-        fig.add_trace(go.Scatter(x=future_dates, y=future['yhat'], name='Прогноз'))
-        fig.update_layout(title=f"{param} в {city} {period}", xaxis_title="Дата", yaxis_title=y_label)
-        fig.update_xaxes(range=[datetime.now(), end_date])
-        bot.send_photo(chat_id=message.chat.id, photo=fig.to_image(format='png'))  # Отправляем график пользователю
-        keyboard = types.ReplyKeyboardMarkup(row_width=1, resize_keyboard=True)
-        button = types.KeyboardButton(text="Вернуться к выбору города")
-        keyboard.add(button)
-        bot.send_message(chat_id=message.chat.id, text='Можете ввести город снова', reply_markup=keyboard)
+        if param == 'Температура':
+            future_tmin = model_tmin.predict(pd.DataFrame({"ds": future_dates}))
+            future_tmax = model_tmax.predict(pd.DataFrame({"ds": future_dates}))
+            future_tavg = model_tavg.predict(pd.DataFrame({"ds": future_dates}))
+            fig = go.Figure()
+            # Рисуем график с историческими данными и прогнозом для tmin
+            # fig.add_trace(go.Scatter(x=forecast_data["ds"], y=forecast_data["tmin"], name='Исторические данные tmin'))
+            fig.add_trace(go.Scatter(x=future_dates, y=future_tmin['yhat'], name='Прогноз минимальной температуры'))
+
+            # Рисуем график с историческими данными и прогнозом для tmax
+            # fig.add_trace(go.Scatter(x=forecast_data["ds"], y=forecast_data["tmax"], name='Исторические данные tmax'))
+            fig.add_trace(go.Scatter(x=future_dates, y=future_tmax['yhat'], name='Прогноз максимальной температуры'))
+
+            # Рисуем график с историческими данными и прогнозом для tavg
+            # fig.add_trace(go.Scatter(x=forecast_data["ds"], y=forecast_data["tavg"], name='Исторические данные tavg'))
+            fig.add_trace(go.Scatter(x=future_dates, y=future_tavg['yhat'], name='Прогноз средней температуры'))
+
+            fig.update_layout(title=f"{param} в {city} {period}", xaxis_title="Дата", yaxis_title=y_label)
+            fig.update_xaxes(range=[datetime.now(), end_date])
+            bot.send_photo(chat_id=message.chat.id, photo=fig.to_image(format='png'))  # Отправляем график пользователю
+            keyboard = types.ReplyKeyboardMarkup(row_width=1, resize_keyboard=True)
+            button = types.KeyboardButton(text="Вернуться к выбору города")
+            keyboard.add(button)
+            bot.send_message(chat_id=message.chat.id, text='Можете ввести город снова', reply_markup=keyboard)
+
+            # Отображение графика
+            # fig.show()
+        else:
+            # Получаем прогноз
+            future = model.predict(pd.DataFrame({"ds": future_dates}))
+            print('Complete for User')
+            # Рисуем график с историческими данными и прогнозом
+            fig = go.Figure()
+            # fig.add_trace(go.Scatter(x=weather_data.index, y=parameter.values, name='Исторические данные'))
+            fig.add_trace(go.Scatter(x=future_dates, y=future['yhat'], name='Прогноз'))
+            fig.update_layout(title=f"{param} в {city} {period}", xaxis_title="Дата", yaxis_title=y_label)
+            fig.update_xaxes(range=[datetime.now(), end_date])
+            bot.send_photo(chat_id=message.chat.id, photo=fig.to_image(format='png'))  # Отправляем график пользователю
+            keyboard = types.ReplyKeyboardMarkup(row_width=1, resize_keyboard=True)
+            button = types.KeyboardButton(text="Вернуться к выбору города")
+            keyboard.add(button)
+            bot.send_message(chat_id=message.chat.id, text='Можете ввести город снова', reply_markup=keyboard)
 
 
     else:
